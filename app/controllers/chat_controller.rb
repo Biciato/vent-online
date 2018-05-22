710e2466ea12217f9acb7effc5fb5c981dab1d4f
Biciato <leandrobiciato58@gmail.com>
2018-05-22 15:19:22 -0300

    enabling access chat

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,12 +1,6 @@
 class ChatController < ApplicationController
 
   def room
-    # checks if the user is online, e.g. the online attribute is true on the db. If it's not, redirect_to
-    # to the root 
-    if current_user.online_check
-      redirect_to root_path
-    else
-      current_user.online!
-    end
+
   end
 end
